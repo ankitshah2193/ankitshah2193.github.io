12477278587f45f0f0f75cb78ad1b8ae7d0fc3ee
Prerna Shah <prerna.shah@thoughtworks.com>
2018-12-17 15:43:59 +0530

    Bootstrap used for styling

--- src/components/currentOverDetails.js
+++ src/components/currentOverDetails.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/currentOverDetails.css';
 
 
 class CurrentOverDetails extends React.Component{
     render(){
         return (
-            <div className="current-over-details">
-                <div>
+            <div className="row current-over-details">
+                <div className="col-sm-12">
                     This Over : 2 3 4 5 6 2
                     
                 </div>
-                <div>
+                <div className="col-sm-12">
                     Bowler :  Abhilash 
                 </div>
             </div>
