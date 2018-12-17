3b178fd3fdfa8cb97a644f7c2c12114bbc06e55b
Prerna Shah <prerna.shah@thoughtworks.com>
2018-12-17 15:33:31 +0530

    current over details component done

---
+++ src/components/currentOverDetails.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../styles/currentOverDetails.css';
+
+
+class CurrentOverDetails extends React.Component{
+    render(){
+        return (
+            <div className="current-over-details">
+                <div>
+                    This Over : 2 3 4 5 6 2
+                    
+                </div>
+                <div>
+                    Bowler :  Abhilash 
+                </div>
+            </div>
+        );
+    }
+}
+
+export default CurrentOverDetails; 
