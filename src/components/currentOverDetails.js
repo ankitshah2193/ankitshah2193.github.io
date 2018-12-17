3719856f5f8630ebb0d77ead23c717556ed595b4
Prerna Shah <prerna.shah@thoughtworks.com>
2018-12-17 17:04:13 +0530

    Connect currentOverDetails component to reducer

--- src/components/currentOverDetails.js
+++ src/components/currentOverDetails.js
@@ -1,22 +1,40 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/currentOverDetails.css';
+import {connect} from 'react-redux';
 
-
+var currentOverDetails =" ";
 class CurrentOverDetails extends React.Component{
+    
+      renderOverDetails(currentOverArray) {
+        var index =0;
+        for (index = 0; index < currentOverArray.length; index++) { 
+            currentOverDetails = currentOverDetails + currentOverArray[index] + " ";
+        } 
+        return currentOverDetails;
+      }
+ 
+
     render(){
         return (
             <div className="row current-over-details">
                 <div className="col-sm-12">
-                    This Over : 2 3 4 5 6 2
-                    
+                    This Over: {this.renderOverDetails(this.props.currentOverDetails)}
                 </div>
                 <div className="col-sm-12">
-                    Bowler :  Abhilash 
+                    Bowler: {this.props.currentBowler}
                 </div>
             </div>
         );
     }
+
 }
 
-export default CurrentOverDetails; 
+const mapStateToProp = state =>{
+    return {
+        currentBowler: state.currentBowler,
+        currentOverDetails: state.currentOverDetails
+    }
+}
+
+export default connect(mapStateToProp)(CurrentOverDetails);
