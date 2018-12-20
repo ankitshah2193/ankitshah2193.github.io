d2e649e2cbef7b26712246ccad4c368abf457987
Ankit Shah <ankitkumar.shah@thoughtworks.com>
2018-12-20 17:08:35 +0530

    Updates

---
+++ precache-manifest.0f130decf80558e551d3fb96d3686fa9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b07851c932f4ad0a3de7",
+    "url": "/cricket/static/js/runtime~main.b07851c9.js"
+  },
+  {
+    "revision": "5a59dc322c7db53a1285",
+    "url": "/cricket/static/js/main.5a59dc32.chunk.js"
+  },
+  {
+    "revision": "b957402dc421c1f99d3b",
+    "url": "/cricket/static/js/1.b957402d.chunk.js"
+  },
+  {
+    "revision": "5a59dc322c7db53a1285",
+    "url": "/cricket/static/css/main.eadf6be9.chunk.css"
+  },
+  {
+    "revision": "b957402dc421c1f99d3b",
+    "url": "/cricket/static/css/1.f58a6193.chunk.css"
+  },
+  {
+    "revision": "5b1d392e94060a9c177313ccab06f082",
+    "url": "/cricket/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ac5eb10aec2935dd2f83b1cd822179ca.js"
+  "/cricket/precache-manifest.0f130decf80558e551d3fb96d3686fa9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/cricket/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
